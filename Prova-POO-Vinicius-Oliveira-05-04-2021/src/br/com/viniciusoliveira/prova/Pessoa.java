9d49114d74018e701336ddca02006d4cefbb6fd4
vini-costa <53922186+vini-costa@users.noreply.github.com>
2021-04-05 09:42:17 -0300

    Adicionado arquivo via upload

---
+++ Prova-POO-Vinicius-Oliveira-05-04-2021/src/br/com/viniciusoliveira/prova/Pessoa.java
@@ -0,0 +1,36 @@
+package br.com.viniciusoliveira.prova;
+
+public class Pessoa {
+
+	private String nome;
+	private String sobrenome;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	
+	
+	//crianto este metodo
+	public String getNomeCompleto() {
+		
+		String nomeCompleto;
+		
+		
+		nomeCompleto = "a";
+		
+		
+		return nomeCompleto; 
+	}
+	
+}
